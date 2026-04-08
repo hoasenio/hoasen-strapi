cdef707bf544b49bc7d9a7ae61a368eaf5a9d953
DxcMint868 <nddminh2021@gmail.com>
2026-04-08 14:30:51 +0700

    Add Docker support with Dockerfile, docker-compose for production, and .dockerignore; update server configuration to support public URL; add Caddyfile for reverse proxy; include PostgreSQL dependency in package.json

--- config/server.js
+++ config/server.js
@@ -1,10 +1,20 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+module.exports = ({ env }) => {
+  const config = {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  };
+
+  const publicUrl = env('PUBLIC_URL', '');
+  if (publicUrl) {
+    config.url = publicUrl;
+    config.proxy = true;
+  }
+
+  return config;
+};
